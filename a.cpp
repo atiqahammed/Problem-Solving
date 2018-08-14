8a26d89480a1990662ca983fd7cd42c081eb3735
atiqahammed <atiqahammedshamim@gmail.com>
2018-08-14 12:27:59 +0600

    problem 1 solved

--- a.cpp
+++ a.cpp
@@ -8,7 +8,7 @@ using namespace std;
 typedef long long ll;
 
 
-ll n;
+//ll n;
 
 
 
@@ -18,7 +18,51 @@ int main(void) {
     freopen("output.txt","w",stdout);
   	#endif
 	
-	ll count = 0;
+	//ll count = 0;
+	
+	
+	
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	int index = s.length();
+	
+	
+	for(int i = 0; i < n; i++) {
+		int type;
+		cin >> type;
+		
+		//cout << i << endl;
+		
+		if(type == 1) {
+			string xx;
+			cin >> xx;
+			
+		//	cout << ">>> " << xx << endl;
+			
+			if(index == 0 && xx.length() > 1) continue;
+			if(xx.length() > 1) {
+			//	cout << "paichi" << endl;
+				s.erase(index-1, 1);
+				index--;
+			} else {
+				s.insert(index, xx);
+				index++;
+			}
+			
+		} else {
+			int tt;
+			cin >> tt;
+			index = tt;
+		
+		}
+		
+	//	cout << s << endl;
+		
+	}
+	
+	cout << s << endl;
 	
 	
 	
