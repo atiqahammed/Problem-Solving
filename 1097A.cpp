c204f78e2ee1ecc08fcfef6ba6097adc9543be0d
Atiq Ahammed <30492609+atiqahammed@users.noreply.github.com>
2019-01-07 17:02:59 +0600

    1097A solved

--- 1097A.cpp
+++ 1097A.cpp
@@ -19,8 +19,24 @@ int main(void) {
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
   	#endif
+	string s, a[5];
 	
+	cin >> s;
+	
+	
+	
+	for(int i = 0; i < 5; i++) {
+		cin >> a[i];
+	}
+	
+	for(int i = 0; i < 5; i++) {
+		if(s[0] == a[i][0] || s[1] == a[i][1]) {
+			cout << "YES" << endl;
+			return 0;	
+		}
+	}
 
+	cout << "NO" << endl;
 	
 	
     return 0;
